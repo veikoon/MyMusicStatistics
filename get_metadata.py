1b749b6ac043645edc2844f82ecd451ae2458832
ialexvo <73938500+ialexvo@users.noreply.github.com>
2020-11-10 12:03:33 +0100

    Add song dictionary generation

--- get_metadata.py
+++ get_metadata.py
@@ -1,16 +1,25 @@
 import eyed3
+import os
 
 class GetMetadata:
 
     def __init__(self):
         self.__PATH = './Library/'
 
-    def show_info(self, mp3_path):
-        audio = eyed3.load(self.__PATH + mp3_path)
-        print(audio.tag.artist)
-        print(audio.tag.album)
-        print(audio.tag.title)
-        print(audio.tag.release_date)
-        if(audio.tag.genre != None):
-            print(str(audio.tag.genre)[3:])
-        print(audio.info.time_secs)
+    def get_songs(self):
+        songs = dict()
+        for filename in os.listdir(self.__PATH):
+            if not filename.endswith(".mp3"):
+                pass
+            song = eyed3.load(self.__PATH + filename)
+            songs[song.tag.title] = {"artist": song.tag.artist, "album": song.tag.album,
+                                         "release_date": str(song.tag.release_date), "genre": song.tag.genre}
+            temp_song = songs[song.tag.title].copy()
+            for key, value in songs[song.tag.title].items():
+                if value is None:
+                    temp_song.pop(key)
+                elif key == "genre":
+                    temp_song["genre"] = str(song.tag.genre).lstrip("()0123456789")
+            songs[song.tag.title] = temp_song
+
+        return(songs)
