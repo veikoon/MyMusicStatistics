ad883014dac50f8e8b21d3a240494c7782b09d6e
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2020-11-10 10:13:21 +0100

    add metadata info

--- get_metadata.py
+++ get_metadata.py
@@ -2,10 +2,15 @@ import eyed3
 
 PATH = './Library/'
 
-def show_info():
-    audio = eyed3.load(PATH + "02 B.Y.O.B..mp3")
+def show_info(mp3_path):
+    audio = eyed3.load(PATH + mp3_path)
     print(audio.tag.artist)
     print(audio.tag.album)
     print(audio.tag.title)
+    print(audio.tag.release_date)
+    if(audio.tag.genre != None):
+        print(str(audio.tag.genre)[3:])
+    print(audio.info.time_secs)
 
-show_info()
+show_info("2-06 Never Too Late.mp3")
+show_info("02 B.Y.O.B..mp3")
