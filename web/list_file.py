a8e24084d287a7096864e4f8e581ccd4ef263c13
Vincent Monnot <vincent.monnot@edu.esiee.fr>
2020-11-20 11:10:35 +0100

    add list

--- web/list_file.py
+++ web/list_file.py
@@ -6,10 +6,30 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+import dash_table
 from dash.dependencies import Input, Output, State
 
 from web.navbar import nav
 from web.app import app
+from get_metadata import GetMetadata
+
+get_metada = GetMetadata()
+songs = get_metada.get_songs()
+
+keys = []
+for keys,values in songs.items():
+    keys = values.keys()
+
+tab = list()
+for key in songs.keys():
+    tab.append(songs[key])
+
+table = dash_table.DataTable(
+    id='table',
+    columns=[{"name": i, "id": i} for i in keys],
+    style_cell={'textAlign': 'center'},
+    data=tab,
+)
 
 collapse = html.Div(
     [
@@ -20,7 +40,7 @@ collapse = html.Div(
             color="primary",
         ),
         dbc.Collapse(
-            dbc.Card(dbc.CardBody("This content is hidden in the collapse")),
+            table,
             id="collapse",
         ),
     ]
