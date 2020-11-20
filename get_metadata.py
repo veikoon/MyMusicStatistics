3003ad32b2214472ddd93a02893b50f20d6c2455
ialexvo <alex.vo@edu.esiee.fr>
2020-11-20 10:37:40 +0100

    Add Metadata tags

--- get_metadata.py
+++ get_metadata.py
@@ -1,5 +1,7 @@
 import eyed3
 import os
+import re
+
 
 class GetMetadata:
 
@@ -12,18 +14,22 @@ class GetMetadata:
             if not filename.endswith(".mp3"):
                 pass
             song = eyed3.load(self.__PATH + filename)
-            songs[song.tag.title] = {"artist": song.tag.artist,
-                                     "album": song.tag.album,
-                                     "release_date": str(song.tag.release_date),
-                                     "genre": song.tag.genre,
-                                     "publisher": song.tag.publisher,
-                                     "composer": song.tag.composer}
+            songs[song.tag.title] = {"artist": str(song.tag.artist),
+                                     "album": str(song.tag.album),
+                                     "release_date": int(str(song.tag.release_date)),
+                                     "genre": str(song.tag.genre),
+                                     "publisher": str(song.tag.publisher),
+                                     "composer": str(song.tag.composer),
+                                     "duration": float(str(song.info.time_secs)),
+                                     "bit_rate": str(song.info.bit_rate_str)}
             temp_song = songs[song.tag.title].copy()
             for key, value in songs[song.tag.title].items():
                 if value is None:
                     temp_song.pop(key)
                 elif key == "genre":
                     temp_song["genre"] = str(song.tag.genre).lstrip("()0123456789")
+                elif key == "bit_rate":
+                    temp_song["bit_rate"] = str(list(map(int, re.findall("\d+", temp_song["bit_rate"])))).strip("[]")
                 elif temp_song["album"] == "[non-album tracks]":
                     temp_song["album"] = "Single"
             songs[song.tag.title] = temp_song
