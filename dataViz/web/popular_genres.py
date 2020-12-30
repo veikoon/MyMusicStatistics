5d22298bf4318dc1a55ed28c5b4c5a024d11727a
ialexvo <alex.vo@edu.esiee.fr>
2020-12-30 15:28:39 +0100

    Update popular genres dashboard

--- dataViz/web/popular_genres.py
+++ dataViz/web/popular_genres.py
@@ -14,13 +14,11 @@ class PopularGenres:
 
     def get_layout(self, songs):
         sub_dict = dict()
-        genres = list()
+        genres = ["Rock", "Other", "Alternative", "Metal", "R&B", "Classic", "Hip-Hop", "Pop"]
         for song in songs:
             if "release_date" in songs[song] and "genre" in songs[song]:
                 if songs[song]["release_date"] not in sub_dict:
                     sub_dict[songs[song]["release_date"]] = dict()
-                if songs[song]["genre"] not in genres:
-                    genres.append(songs[song]["genre"])
 
         for year in sub_dict:
             for genre in genres:
@@ -28,17 +26,25 @@ class PopularGenres:
             for song in songs:
                 if "release_date" in songs[song] and "genre" in songs[song]:
                     if year == songs[song]["release_date"]:
-                        sub_dict[year][songs[song]["genre"]] += 1
-
+                        increment_other = True
+                        for genre in genres:
+                            if genre in songs[song]["genre"]:
+                                sub_dict[year][genre] += 1
+                                increment_other = False
+                                break
+                        if increment_other:
+                            sub_dict[year]["Other"] += 1
 
         popular_genres = pd.concat({k: pd.Series(v) for k, v in sub_dict.items()}).reset_index()
-        popular_genres.columns = ['year', 'genre','count']
+        popular_genres.columns = ['year', 'genre', 'count']
         year_sort = popular_genres.sort_values(by="year", ascending=True)
 
         fig = px.bar(
             year_sort,
             x="genre",
             y="count",
+            color="genre",
+            range_y=[0,50],
             animation_frame="year",
         )
 
